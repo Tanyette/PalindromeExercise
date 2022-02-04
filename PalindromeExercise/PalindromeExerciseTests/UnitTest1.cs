9eb883c085af3387379a9a3501ebd7328fdd9c3a
Tanyette Wiggison <bwaffiliates@yahoo.com>
2022-02-03 20:42:43 -0700

    initial commit

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,49 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+
+
+
+        [Theory]
+        [InlineData("civic", true)]
+        [InlineData("deleveled", true)]
+        [InlineData("devoved", true)]
+        [InlineData("Hannah", true)]
+        [InlineData("deified", true)]
+        [InlineData("racecar", true)]
+        public void PalindromeTester(string word, bool expected)
         {
 
+            // Arrange- prepare the code i need
+            //          in order to call the method.
+            var wordSmith = new WordSmith();
+
+            // Act
+            var actual = wordSmith.IsAPalindrome(word);
+
+            // Assert
+            Assert.Equal(expected, actual);
+
+
+
+
+
+
+
+
+
+
+
         }
+
+
+
+
+
     }
 }
